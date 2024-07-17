b4b9955edf4f3222c138d20870c0a416e7261a5b
Wallyson Alessandro Jeronimo Nabarro Lopes <wallyson.lopes.colaborador@incra.gov.br>
2024-07-17 17:54:42 -0300

    atualização do environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:7277/api/',  
-    // apiUrl: 'https://bracofortedosenhorsiao.azure-api.net/api/',    
+    // apiUrl: 'https://localhost:44374/api/',  
+    apiUrl: 'https://bracofortedosenhorsiao.azure-api.net/api/',    
     keyApi: '05ae2e867bb547e1a16e8a4ad06d69b3' 
   };
   
