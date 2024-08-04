b4f63c6e9f5ca0c1f6394bd500f8443ac9581edf
Wallyson Alessandro Jeronimo Nabarro Lopes <wallyson.lopes.colaborador@incra.gov.br>
2024-08-03 22:02:03 -0300

    Ajustes: cache, cep

--- src/app/cepService/cep.service.ts
+++ src/app/cepService/cep.service.ts
@@ -2,19 +2,28 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Cep } from '../interfaces/Cep';
+import { environment } from '../../environments/environment';
+import { CabecalhoService } from '../services/cabecalho/cabecalho.service';
+import { CepDto } from './CepDto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CepService {
+  
+  private readonly uri: string = `${environment.apiUrl}Cep/`;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private cabecalhoServicos: CabecalhoService) { }
 
   getCep(cep: string): Observable<Cep> {
 
-    const url = `http://viacep.com.br/ws/${cep}/json/`;
+    let _headers = this.cabecalhoServicos.gerarCabecalho('');
 
-    return this.http.get<Cep>(url);
+    const Cep: CepDto = {
+      Cep: cep
+    };
+    
+    return this.http.post<Cep>(`${this.uri}cep`, Cep, { headers: _headers });
   }
 
 }
